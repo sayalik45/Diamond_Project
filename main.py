372f0ee8cbe624f6578f62323795a5147002b6c3
unknown <sayalikadam1207@gmail.com>
2023-03-16 17:53:59 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,37 @@
+
+import flask
+from flask import Flask,render_template,request
+import numpy as np
+from utils import diamond_prediction
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+@app.route('/get_data',methods = ['POST','GET'])
+def get_data():
+
+    try :
+        if request.method == "POST":
+            data = request.form
+            class_obj = diamond_prediction(data)
+            result = class_obj.price_prediction()
+            return render_template('index.html',prediction = result)
+
+        else :
+            print(f"wrong method")
+            return "Wrong method"
+    except :
+        print(traceback.print_exc())
+        return render_template('result.html')
+
+    
+
+
+    
+    
+
+if __name__ == "__main__":
+    app.run(host = '0.0.0.0',port = 5050,debug = True)
---
+++ utils.py
@@ -0,0 +1,61 @@
+import pandas as pd 
+import numpy as np 
+import pickle
+import json
+import sklearn
+
+class diamond_prediction():
+    def __init__(self,data):
+        self.data = data
+
+    def loading_files(self):
+
+        with open(r'artifacts/diamond_model.pkl','rb') as file :
+            self.model = pickle.load(file)
+
+        with open(r'artifacts/diamond_scale.pkl','rb') as file :
+            self.scaler = pickle.load(file)
+
+        with open(r'artifacts/project_data.json','r') as file :
+            self.project_data = json.load(file)
+
+    def price_prediction(self):
+
+        self.loading_files()
+
+        carat = self.data['html_carat']
+        color = self.data['html_color']
+        cut = self.data['html_cut']
+        clarity = self.data['html_clarity']
+        depth = self.data['html_depth']
+        table = self.data['html_table']
+        x = self.data['html_x']
+        y = self.data['html_y']
+        z = self.data['html_z']
+
+        user_data = np.zeros(len(self.project_data['column_names']))
+
+        user_data[0]=carat
+        #cut
+        user_data[1]=self.project_data['cut'][cut]
+
+        #clarity
+        user_data[2]=self.project_data['clarity'][clarity]
+
+        user_data[3]=depth
+        user_data[4]=table
+        user_data[5]=x
+        user_data[6]=y
+        user_data[7]=z
+
+        #color
+        search_color='color_'+color ##(color_E)
+        index=np.where(self.project_data['column_names']==search_color)
+        user_data[index]=1
+
+        ## prediction
+        scale=self.scaler.transform([user_data])
+        result = self.model.predict(scale)[0]
+        return result
+
+
