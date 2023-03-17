044f50888002435d5f1f0682ddd396dbd8688e30
unknown <sayalikadam1207@gmail.com>
2023-03-16 18:53:23 -0700

    library delete

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
-import pandas as pd 
-import numpy as np 
+#import pandas as pd 
+#import numpy as np 
 import pickle
 import json
-import sklearn
+#import sklearn
 
 class diamond_prediction():
     def __init__(self,data):
