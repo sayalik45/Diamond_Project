25db87693440f1642505d7063401e339f5d24925
unknown <sayalikadam1207@gmail.com>
2023-03-16 19:41:38 -0700

    modify main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import flask
 from flask import Flask,render_template,request
 import numpy as np
-import traceback
+
 from utils import diamond_prediction
 
 app = Flask(__name__)
@@ -13,20 +13,11 @@ def index():
 
 @app.route('/get_data',methods = ['POST','GET'])
 def get_data():
+    data = request.form
+    class_obj = diamond_prediction(data)
+    result = class_obj.price_prediction()
+    return render_template('index.html',prediction = result)
 
-    try :
-        if request.method == "POST":
-            data = request.form
-            class_obj = diamond_prediction(data)
-            result = class_obj.price_prediction()
-            return render_template('index.html',prediction = result)
-
-        else :
-            print(f"wrong method")
-            return "Wrong method"
-    except :
-        print(traceback.print_exc())
-        return render_template('result.html')
 
     
 
